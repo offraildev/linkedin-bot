01bd11f5f76c24c8ffa6020e7150c2718796c1cd
Sajid Mashroor <offrail@Sajids-MacBook-Air.local>
2023-09-24 18:23:18 +0530

    CLI options, data extraction scripts, logging.
        - add logging methods.
        - move data extractions scripts to folder.
        - add CLI options to scriptse.

---
+++ extraction_scripts/extract_company_description.py
@@ -0,0 +1,43 @@
+import os
+import json
+import time
+import argparse
+from pathlib import Path
+
+from dotenv import load_dotenv
+from loggers import log_message
+from request_scripts.company_description import request_company_description
+
+load_dotenv()
+JOB_IDS_FILE = os.getenv("JOB_IDS_FILE")
+COMPANY_METAS = os.getenv("COMPANY_METAS")
+EXTRACT_COMPANY_DESC_LOG = os.getenv("EXTRACT_COMPANY_DESC_LOG")
+
+
+def extract_company_description(job_ids: list[int], sleep_time: int) -> None:
+    result = {}
+    for id in job_ids:
+        result[id] = request_company_description(id)
+        log_message(EXTRACT_COMPANY_DESC_LOG, f"last meta collected from index: {id}")
+        time.sleep(sleep_time)
+    Path(COMPANY_METAS).write_text(json.dumps(result, indent=4))
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        prog="extract_company_descriptions",
+        description="Extract company descriptions and save to file.",
+    )
+    parser.add_argument(
+        "--sleep_time",
+        type=int,
+        help="Sleep time to use in-between request calls.",
+        default=8,
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    job_ids = list(map(int, Path(JOB_IDS_FILE).read_text().splitlines()))
+    extract_company_description(job_ids, args.sleep_time)
